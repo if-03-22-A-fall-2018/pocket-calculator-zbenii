d041f7332b23b3ad0d5736d989e8a66cdcd444a7
zbenii <besic.beni@gmx.at>
2018-10-07 10:53:40 +0200

    Finished the programm

--- Calculator.c
+++ Calculator.c
@@ -1,5 +1,18 @@
-//Benjamin Besic 2AHIF
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: <2AHIF>
+ * ---------------------------------------------------------
+ * Exercise Number: 2
+ * Title:			<Pocket Calculator>
+ * Author:			<Benjamin Besic>
+ * ----------------------------------------------------------
+ * Description:
+ * <It's a simple calculator with 2 operands.
+ *  You can Add,Subtract,Multiply and Divide>
+ * ----------------------------------------------------------
+ */
+
 #include <stdio.h>
+#include <float.h>
 
 int Menu(int* c)
 {
@@ -18,23 +31,58 @@ int Menu(int* c)
 switch (*c)
 {
   case 1:
+  GetOperands(&o1,&o2);
   Add(o1,o2,&result);
   OutputResult(result);
+  printf(" \n");
   break;
   case 2:
+  GetOperands(&o1,&o2);
+  Subtract(o1,o2,&result);
+  OutputResult(result);
+  printf(" \n");
   break;
   case 3:
+  GetOperands(&o1,&o2);
+  Multiply(o1,o2,&result);
+  OutputResult(result);
+  printf(" \n");
   break;
   case 4:
+  GetOperands(&o1,&o2);
+  if(o2!=0)
+  {
+    Divide(o1,o2,&result);
+    OutputResult(result);
+    printf(" \n");
+  }
+  else
+  {
+    printf("Division by Zero! \n");
+    printf(" \n");
+  }
   break;
+  default:
+  printf("Input not allowed, please try again\n");
+  printf(" \n");
 
 }
 }
-
 
 void OutputResult(double result)
 {
-  printf("%lf\n",result );
+  if(result>DBL_MAX)
+  {
+    printf("Number overflow! \n");
+  }
+  else if(result<-DBL_MAX)
+  {
+    printf("Number underflow! \n");
+  }
+  else
+  {
+  printf("The result is: %lf\n", result);
+  }
 }
 
 void GetOperands(double* x,double* y)
@@ -47,14 +95,23 @@ void GetOperands(double* x,double* y)
 
 void Add(double x,double y,double* result)
 {
-  GetOperands(&x,&y);
+
   *result=x+y;
 }
 
 void Subtract(double x, double y, double* result)
 {
-  GetOperands(&x,&y);
   *result=x-y;
+}
+
+void Multiply(double x, double y,double* result)
+{
+  *result=x*y;
+}
+
+void Divide(double x, double y, double* result)
+{
+  *result= x/y;
 }
 
 int main(int argc, char const *argv[])
